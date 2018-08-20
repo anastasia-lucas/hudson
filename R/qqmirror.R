24f87954945fd5595e5961680bff476c31a75685
Anastasia Lucas <anastasia.lucas@pennmedicine.upenn.edu>
2018-08-20 14:12:06 -0400

    Add freey

--- R/qqmirror.R
+++ R/qqmirror.R
@@ -13,6 +13,7 @@
 #' @param highlight_name list of names to highlight, dataframe must include a Name column
 #' @param highlight_p p-value threshold to highlight
 #' @param highlighter highlighter color
+#' @param freey allow y-axes to scale with data
 #' @param annotate_name list of names to annotate, dataframe must include a Name column
 #' @param annotate_p p-value threshold to annotate, dataframe must include a Name column
 #' @param line draw a red line at pvalue threshold (observed)
@@ -30,7 +31,7 @@
 #' qqdat <- data.frame(pvalue=gwas$pvalue, Color=gwas$Frame)
 #' qqmirror(top=qqdat, bottom=qqdat, opacity=0.6, splittop="Color", splitbottom="Color", toptitle="Example: Top", bottomtitle="Example: Bottom")
 
-qqmirror <- function(top, bottom, CI=0.95, opacity=1, groupcolors, splittop=NULL, splitbottom=NULL, highlight_p, highlight_name, annotate_p, annotate_name, highlighter="red", tline, bline, background, toptitle=NULL, bottomtitle=NULL, file="qqmirror", wi=6, hgt=12, hgtratio=0.5, res=300){
+qqmirror <- function(top, bottom, CI=0.95, opacity=1, groupcolors, splittop=NULL, splitbottom=NULL, highlight_p, highlight_name, annotate_p, annotate_name, highlighter="red", freey=FALSE, tline, bline, background, toptitle=NULL, bottomtitle=NULL, file="qqmirror", wi=6, hgt=12, hgtratio=0.5, res=300){
 
   arglist <- list(d=top, CI=CI, opacity=opacity, splitby=splittop, highlighter=highlighter)
   if(!missing(tline)){arglist <- c(arglist, line=tline)}
@@ -53,8 +54,15 @@ qqmirror <- function(top, bottom, CI=0.95, opacity=1, groupcolors, splittop=NULL
   if(!missing(annotate_name)){arglist <- c(arglist, annotate_name=annotate_name)}
   if(!missing(background)){arglist <- c(arglist, background=background)}
   p2 <- do.call(qqunif, arglist)
-  p2 <- p2 + scale_y_reverse()
   
+  if(freey==FALSE){
+    p1 <- p1 + scale_y_continuous(limits = c(0, max(max(-log10(top$pvalue)), max(-log10(bottom$pvalue)))))
+    yax <- max(max(-log10(top$pvalue)), max(-log10(bottom$pvalue)))
+    p2 <- p2 + scale_y_reverse(limits=c(yax,0))
+  } else {
+    p2 <- p2 + scale_y_reverse()
+    
+  }
   #Save
   print(paste("Saving plot to ", file, ".png", sep=""))
   p <- grid.arrange(arrangeGrob(p1, top=toptitle), arrangeGrob(p2, bottom=bottomtitle), padding=0, heights=c(hgtratio,1-hgtratio))
